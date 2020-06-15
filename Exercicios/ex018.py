8e48931d969ac83fef882c892e4af3c75c6197db
RanatoFarofa <renatosoaresgalina@gmail.com>
2020-06-15 10:29:34 -0400

    conversão para radiano e acrecentou o calc da tangente. ex0018

--- Exercicios/ex018.py
+++ Exercicios/ex018.py
@@ -1,6 +1,8 @@
 #ler um angulo e calcular o sen e cos
 import math
 angulo = float(input('Digite o angulo para calcularmos o seno e coseno : '))
-coseno = math.cos(angulo)
-seno = math.sin(angulo)
-print('o angulo de {}° possui o seno de {:.4f}° e coseno de {:.4f}°'.format(angulo,seno,coseno))
+coseno = math.cos(math.radians(angulo))
+seno = math.sin(math.radians(angulo))
+print('o angulo de {}° possui o seno de {:.2f}° e coseno de {:.2f}°'.format(angulo,seno,coseno))
+tangente = math.tan(math.radians(angulo))
+print('A tangente é: {:.2f}° '.format(tangente))
