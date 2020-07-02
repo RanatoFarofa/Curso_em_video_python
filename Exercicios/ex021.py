dfd95abf1db35ef7af9282541bb8c0020a972719
RanatoFarofa <renatosoaresgalina@gmail.com>
2020-07-02 14:49:07 -0400

    Mudança de caminho para o audio2 do ex021

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -1,6 +1,6 @@
 #faça um programa que abra e reproduza um arquvio MP3.
 import pygame
 pygame.mixer.init()
-pygame.mixer.music.load('C:/Users/renat/Documents/CursoEmVideo/python/Exercicios/audio2.mp3')
+pygame.mixer.music.load('/Users/renatogalina/Documents/Documents/CursoEmVideo/python/Exercicios/audio2.mp3')
 pygame.mixer.music.play()
 parar = input('Quando quiser parar digite qualquer coisa.....')
